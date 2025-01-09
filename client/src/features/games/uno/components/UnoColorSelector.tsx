c22b0f19d841109ae9da63f74694228d11d22058
Nathan <52774360+NDB-7@users.noreply.github.com>
2025-01-08 17:02:04 -0800

    Cleaned up UnoGame.tsx code

---
+++ client/src/features/games/uno/components/UnoColorSelector.tsx
@@ -0,0 +1,51 @@
+import { useContext } from "react";
+import UnoCard from "./UnoCard";
+import ColorSelectorContext from "../context/ColorSelectorContext";
+import { SessionType } from "@/types";
+
+const [colorSelector, setColorSelector] = useContext(ColorSelectorContext);
+
+export default function UnoColorSelector({
+  session,
+}: {
+  session?: SessionType;
+}) {
+  return (
+    <div
+      className="absolute bg-black w-full h-full z-20 bg-opacity-50"
+      onClick={() => setColorSelector(null)}
+    >
+      <p className="absolute text-3xl font-bold text-white left-1/2 -translate-x-1/2 mt-6">
+        Pick a color
+      </p>
+      <div className="absolute left-1/2 -translate-x-1/2 pt-28 pb-64 h-full flex flex-col justify-between">
+        <UnoCard
+          face={colorSelector}
+          color="red"
+          session={session}
+          colorSelect
+        />
+        <UnoCard
+          face={colorSelector}
+          color="green"
+          session={session}
+          colorSelect
+        />
+      </div>
+      <div className="absolute left-1/2 -translate-x-1/2 flex gap-40 -translate-y-1/2 top-1/2 pb-36 px-64">
+        <UnoCard
+          face={colorSelector}
+          color="blue"
+          session={session}
+          colorSelect
+        />
+        <UnoCard
+          face={colorSelector}
+          color="yellow"
+          session={session}
+          colorSelect
+        />
+      </div>
+    </div>
+  );
+}
